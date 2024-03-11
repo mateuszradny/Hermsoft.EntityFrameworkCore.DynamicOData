fc16b0dba87cc39c2f4fa18add2d1b7430800e67
Mateusz Radny <mateuszradnywrz@gmail.com>
2024-03-11 17:15:16 +0100

    added odata controllers (without implementation yet)

---
+++ src/Hermsoft.EntityFrameworkCore.DynamicOData/Controllers/ODataController`3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Deltas;
+
+namespace Hermsoft.EntityFrameworkCore.DynamicOData.Controllers
+{
+    public class ODataController<TEntity, TKey1, TKey2> : ReadOnlyODataController<TEntity, TKey1, TKey2>
+        where TEntity : class
+    {
+        public async Task<IActionResult> Post([FromBody] TEntity entity)
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Put(TKey1 key1, TKey2 key2, TEntity entity)
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Patch(TKey1 key1, TKey2 key2, [FromBody] Delta<TEntity> entity)
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Delete(TKey1 key1, TKey2 key2)
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Hermsoft.EntityFrameworkCore.DynamicOData/Controllers/ReadOnlyODataController`2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+
+namespace Hermsoft.EntityFrameworkCore.DynamicOData.Controllers
+{
+    public class ReadOnlyODataController<TEntity, TKey> : ODataController
+        where TEntity : class
+    {
+        [EnableQuery]
+        public async Task<IQueryable<TEntity>> Get()
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+
+        [EnableQuery]
+        public async Task<TEntity> Get(TKey key)
+        {
+            await Task.Yield();
+            throw new NotImplementedException();
+        }
+    }
+}
