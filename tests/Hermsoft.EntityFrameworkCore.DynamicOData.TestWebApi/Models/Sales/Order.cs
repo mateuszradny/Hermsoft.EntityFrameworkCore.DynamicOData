61901e13587d763fab6c322a9b55ae20450dfa3c
Mateusz Radny <mateuszradnywrz@gmail.com>
2024-03-27 12:55:40 +0100

    - added more odata query tests;

--- tests/Hermsoft.EntityFrameworkCore.DynamicOData.TestWebApi/Models/Sales/Order.cs
+++ tests/Hermsoft.EntityFrameworkCore.DynamicOData.TestWebApi/Models/Sales/Order.cs
@@ -1,12 +1,15 @@
-﻿namespace Hermsoft.EntityFrameworkCore.DynamicOData.TestWebApi.Models.Sales
+﻿using Hermsoft.EntityFrameworkCore.DynamicOData.TestWebApi.Models.Identity;
+
+namespace Hermsoft.EntityFrameworkCore.DynamicOData.TestWebApi.Models.Sales
 {
     public class Order
     {
-        public required int Id { get; set; }
-        public required Guid UserId { get; set; }
-        public required DateTimeOffset OderedOn { get; set; }
-        public required decimal TotalPrice { get; set; }
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTimeOffset OderedOn { get; set; }
+        public decimal TotalPrice { get; set; }
 
+        public User User { get; set; }
         public ICollection<OrderProduct> OrderProducts { get; private set; } = new HashSet<OrderProduct>();
     }
 }
--- tests/Hermsoft.EntityFrameworkCore.DynamicOData.Tests/ODataComplexQueriesTests.cs
+++ tests/Hermsoft.EntityFrameworkCore.DynamicOData.Tests/ODataComplexQueriesTests.cs
@@ -1,4 +1,7 @@
-﻿using Hermsoft.EntityFrameworkCore.DynamicOData.Tests.Common;
+﻿using FluentAssertions;
+using Hermsoft.EntityFrameworkCore.DynamicOData.Tests.Common;
+using Hermsoft.EntityFrameworkCore.DynamicOData.TestWebApi.Models.Sales;
+using System.Net.Http.Json;
 
 namespace Hermsoft.EntityFrameworkCore.DynamicOData.Tests
 {
@@ -11,37 +14,60 @@ namespace Hermsoft.EntityFrameworkCore.DynamicOData.Tests
         [Fact]
         public async Task Get_QueryWithSelect_ReturnsOnlySelectedFields()
         {
-            throw new NotImplementedException();
+            var response = await HttpClient.GetAsync($"/odata/{nameof(Order)}?$select={nameof(Order.TotalPrice)}");
+            response.Should().BeSuccessful();
+
+            var usersResult = await response.Content.ReadFromJsonAsync<ODataResult<Order>>();
+            usersResult!.value.Select(x => x.Id).Should().AllBeEquivalentTo(0);
+            usersResult!.value.Select(x => x.UserId).Should().AllBeEquivalentTo(Guid.Empty);
+            usersResult!.value.Select(x => x.OderedOn).Should().AllBeEquivalentTo((DateTimeOffset)default);
         }
 
         [Fact]
         public async Task Get_QueryWithExpand_ReturnsCollectionWithExpandedEntities()
         {
-            throw new NotImplementedException();
+            var response = await HttpClient.GetAsync($"/odata/{nameof(Order)}?$expand={nameof(Order.User)}");
+            response.Should().BeSuccessful();
+
+            var usersResult = await response.Content.ReadFromJsonAsync<ODataResult<Order>>();
+            usersResult!.value.Select(x => x.User).Should().NotContainNulls();
         }
 
         [Fact]
         public async Task Get_QueryWithFilter_ReturnsMatchedEntities()
         {
-            throw new NotImplementedException();
+            var response = await HttpClient.GetAsync($"/odata/{nameof(Order)}?$filter={nameof(Order.TotalPrice)} gt 1");
+            response.Should().BeSuccessful();
+
+            var usersResult = await response.Content.ReadFromJsonAsync<ODataResult<Order>>();
+            usersResult!.value.Should().HaveCountGreaterThan(0);
         }
 
         [Fact]
         public async Task Get_QueryWithOrderBy_ReturnsSortedEntities()
         {
-            throw new NotImplementedException();
+            var response = await HttpClient.GetAsync($"/odata/{nameof(Order)}?$orderby={nameof(Order.TotalPrice)}");
+            response.Should().BeSuccessful();
+
+            var usersResult = await response.Content.ReadFromJsonAsync<ODataResult<Order>>();
+            usersResult!.value.Select(x => x.TotalPrice).Should().BeInAscendingOrder();
         }
 
         [Fact]
         public async Task Get_QueryWithGroupBy_ReturnsGroupedEntities()
         {
-            throw new NotImplementedException();
+            var response = await HttpClient.GetAsync($"/odata/{nameof(Order)}?$apply=groupby(({nameof(Order.UserId)}), aggregate({nameof(Order.TotalPrice)} with sum as total))");
+            response.Should().BeSuccessful();
+
+            var usersResult = await response.Content.ReadFromJsonAsync<ODataResult<Order>>();
+            usersResult!.value.Should().HaveCountGreaterThan(0);
         }
 
         [Fact]
         public async Task Get_ComplexQuery_ReturnsValidResult()
         {
-            throw new NotImplementedException();
+            var response = await HttpClient.GetAsync($"/odata/{nameof(Order)}?$select={nameof(Order.TotalPrice)}&$filter={nameof(Order.UserId)} ne null&$orderby={nameof(Order.TotalPrice)} desc&$expand={nameof(Order.User)}");
+            response.Should().BeSuccessful();
         }
     }
 }
